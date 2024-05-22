34fccdfb87f3b18bd7a514982ee7f8ae7f192ba2
xx-hmi <165568150+xx-hmi@users.noreply.github.com>
2024-05-22 10:45:59 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -3,4 +3,4 @@ const year = today.getUTCFullYear();
 const month = today.getUTCMonth()+1;
 const date = today.getUTCDate();
 
-console.log(year + '年',month + '月',date + '日');
+console.log(year + '年' + month + '月' + date + '日');
