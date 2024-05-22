6ab23e0e613676cd13e147d19f19fdcfa95f9f73
xx-hmi <165568150+xx-hmi@users.noreply.github.com>
2024-05-22 10:46:22 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,6 +0,0 @@
-const today = new Date();
-const year = today.getUTCFullYear();
-const month = today.getUTCMonth()+1;
-const date = today.getUTCDate();
-
-console.log(year + '年' + month + '月' + date + '日');
