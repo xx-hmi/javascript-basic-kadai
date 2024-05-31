253567862330fdaf9515d3a09e56b2a9034424fe
xx-hmi <165568150+xx-hmi@users.noreply.github.com>
2024-05-31 21:54:58 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  const text = document.getElementById('text');
+
+  setTimeout( () => {
+    text.textContent = 'ボタンをクリックしました';
+  }, 2000);
+  
+});
